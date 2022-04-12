884f9f8937856f15fce1ca668da75077ba3b6256
Tobias Freese <wolfshund98@gmail.com>
2022-04-12 20:13:34 +0200

    Extend build script to run automatically
    - Can now grab version from a git tag. The needs to include a version number in the format of v1.0.0 following SemVer rules for the numbers.
    - The script can now create a zip file of the output.
    - Added a few arguments to control the flow.
      - Interactive allow the script to prompt the user for data that hasn't been supplied.
      - Compress creates a zip archive of the output.
      - Version allows a version number to be set via parameter.

--- createPackage.ps1
+++ createPackage.ps1
@@ -1,19 +1,43 @@
+param (
+    [switch]
+    $Interactive = $false,
+    [switch]
+    $Compress = $false,
+    [string]
+    $Version = ""
+)
+
 try {
     Write-Host "Dotnet version $(dotnet --version)";
 }
 catch {
     Write-Host "Dotnet not intalled.";
-    Exit;
+    Exit 1;
 }
 
 #Get version
-$version = Read-Host -Prompt "Package version (SemVer)";
+$FindSemVer = "v(\d+\.\d+\.\d+)";
+if ($Version -eq "") {
+    if ("$(git describe --tags --always)" -match $FindSemVer) {
+        $Version = $Matches[1]
+    }
+    else {
+        if ($Interactive) {
+            $Version = Read-Host -Prompt "Package version (SemVer)"
+        }
+        else {
+            Write-Host "Could not find a fitting version number."
+            Exit 1
+        }
+    }
+}
 
 #Clear package directory
-$packageLocation = "$PSScriptRoot\packageOutput\Behaviour Tree"
+$packageOutput = "$PSScriptRoot\packageOutput"
+$packageLocation = "$packageOutput\Behaviour Tree"
 
-if (Test-Path $packageLocation) {
-    Remove-Item $packageLocation -Recurse
+if (Test-Path $packageOutput) {
+    Remove-Item $packageOutput -Recurse
 }
 
 #Build project
@@ -25,7 +49,7 @@ Copy-Item -Path @( "$PSScriptRoot\README.md" ) -Destination $packageLocation
 #Create package.json
 $json = ConvertTo-Json @{
     name = "com.github.technologicalmayhem.behaviourtree";
-    version = $version;
+    version = $Version;
     description = "Tools to create, edit and run behaviour trees.";
     displayName = "Behaviour Tree";
     unity = "2021.2";
@@ -41,4 +65,9 @@ $json = ConvertTo-Json @{
 
 $json | Out-File -FilePath "$packageLocation\package.json" -Encoding utf8
 
-Write-Host "Package created.";
+if ($Compress) {
+    Compress-Archive -Path "$packageLocation\*" -DestinationPath "$packageOutput\Behaviour Tree.zip"
+    Remove-Item $packageLocation -Recurse
+}
+
+Write-Host "Package creation sucessful."
