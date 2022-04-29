2d81be8e12ae2cc6d5ea917dd122bd5e938dba0b
Tobias Freese <wolfshund98@gmail.com>
2022-04-30 00:27:43 +0200

    Add missing documentation to NodeView event argument classes

--- BehaviourTrees.UnityEditor/Data/Events/ConnectionChangedEventArgs.cs
+++ BehaviourTrees.UnityEditor/Data/Events/ConnectionChangedEventArgs.cs
@@ -6,11 +6,26 @@ using BehaviourTrees.UnityEditor.UIElements;
 
 namespace BehaviourTrees.UnityEditor.Data.Events
 {
+    /// <summary>
+    ///     Provides data for the <see cref="NodeView.ConnectionsChanged" /> event.
+    /// </summary>
     public class ConnectionChangedEventArgs : EventArgs
     {
+        /// <summary>
+        ///     All nodes that have been newly connected to the node.
+        /// </summary>
         public readonly IReadOnlyList<NodeView> ConnectedNodes;
+
+        /// <summary>
+        ///     All nodes that have been disconnected from the node.
+        /// </summary>
         public readonly IReadOnlyList<NodeView> DisconnectedNodes;
 
+        /// <summary>
+        ///     Create a new instance of <see cref="ConnectionChangedEventArgs" />.
+        /// </summary>
+        /// <param name="connectedNodes">All nodes that have been newly connected to the node.</param>
+        /// <param name="disconnectedNodes">All nodes that have been disconnected from the node.</param>
         public ConnectionChangedEventArgs(IEnumerable<NodeView> connectedNodes, IEnumerable<NodeView> disconnectedNodes)
         {
             ConnectedNodes = new ReadOnlyCollection<NodeView>(connectedNodes.ToList());
--- BehaviourTrees.UnityEditor/Data/Events/MovedEventArgs.cs
+++ BehaviourTrees.UnityEditor/Data/Events/MovedEventArgs.cs
@@ -1,13 +1,29 @@
 using System;
+using BehaviourTrees.UnityEditor.UIElements;
 using UnityEngine;
 
 namespace BehaviourTrees.UnityEditor.Data.Events
 {
+    /// <summary>
+    ///     Provides data about a <see cref="NodeView" /> object having moved.
+    /// </summary>
     public class MovedEventArgs : EventArgs
     {
+        /// <summary>
+        ///     The old position of the <see cref="NodeView" />.
+        /// </summary>
         public readonly Vector2 OldPosition;
+
+        /// <summary>
+        ///     The new position of the <see cref="NodeView" />.
+        /// </summary>
         public readonly Vector2 NewPosition;
 
+        /// <summary>
+        ///     Creates a new instance of <see cref="MovedEventArgs" />.
+        /// </summary>
+        /// <param name="oldPosition">The old position of the <see cref="NodeView" />.</param>
+        /// <param name="newPosition">The new position of the <see cref="NodeView" />.</param>
         public MovedEventArgs(Vector2 oldPosition, Vector2 newPosition)
         {
             OldPosition = oldPosition;
